7b4aadfd0079103eb27f9248f2b88eddcdd3a6af
Bo-tech237 <arnaudtechnology.com>
2025-05-09 09:44:09 +0100

    style: Format code for consistency and readability in layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,45 +1,45 @@
-import { Metadata } from 'next';
-import DashboardNav from '@/components/DashboardNav';
+import { Metadata } from "next";
+import DashboardNav from "@/components/DashboardNav";
 import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-} from '@/components/ui/sidebar';
-import { cookies } from 'next/headers';
-import AppSidebar from '@/components/AppSidebar';
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+import AppSidebar from "@/components/AppSidebar";
 
 export const metadata: Metadata = {
-    title: {
-        default: 'Dashboard',
-        template: 'Dashboard|%s',
-    },
-    description: 'Find your dream job in Cameroon!',
+  title: {
+    default: "Dashboard",
+    template: "Dashboard|%s",
+  },
+  description: "Find your dream job in Cameroon!",
 };
 
 interface SettingsLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export default async function SettingsLayout({
-    children,
+  children,
 }: SettingsLayoutProps) {
-    const cookieStore = await cookies();
-    const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true';
-    console.log('cookies:', cookieStore, 'open:', defaultOpen);
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
+  console.log("cookies:", cookieStore, "open:", defaultOpen);
 
-    return (
-        <>
-            <SidebarProvider defaultOpen={defaultOpen}>
-                <AppSidebar variant="inset" />
+  return (
+    <>
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebar variant="inset" />
 
-                <SidebarInset>
-                    <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                        <SidebarTrigger />
+        <SidebarInset>
+          <main className="grid grid-cols-1 gap-4 p-4 pt-0">
+            <SidebarTrigger />
 
-                        {children}
-                    </main>
-                </SidebarInset>
-            </SidebarProvider>
-        </>
-    );
+            {children}
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  );
 }
